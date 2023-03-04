dc950efff115d6bc8b29f0f1c1cc21753e56c2f4
Dmitriy Solodovnikov <tuzserik@gmail.com>
2023-03-04 10:38:45 +0300

    Fixed bad admin auth

--- src/main/java/com/github/itmo/ohp/backend/configuration/R2dbcConfiguration.java
+++ src/main/java/com/github/itmo/ohp/backend/configuration/R2dbcConfiguration.java
@@ -4,13 +4,20 @@ import io.r2dbc.spi.ConnectionFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.r2dbc.connection.init.CompositeDatabasePopulator;
 import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.UUID;
 
 @Configuration
 public class R2dbcConfiguration {
-    @Value("{administrator.username}")
+    @Value("${administrator.username}")
     private String username;
-    @Value("{administrator.password}")
+    @Value("${administrator.password}")
     private String password;
     
     @Bean
@@ -18,17 +25,17 @@ public class R2dbcConfiguration {
         ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
         initializer.setConnectionFactory(connectionFactory);
         
-//        CompositeDatabasePopulator compositeDatabasePopulator = new CompositeDatabasePopulator();
-//        compositeDatabasePopulator.addPopulators(new ResourceDatabasePopulator(new ClassPathResource("schema.sql")));
-//        compositeDatabasePopulator.addPopulators(new ResourceDatabasePopulator(new ClassPathResource("data.sql")));
-//        compositeDatabasePopulator.addPopulators(
-//                new ResourceDatabasePopulator(new ByteArrayResource(String.format(
-//                        "insert into users values (\'%s\', \'%s\', \'%s\', \'%s\');",
-//                        "root", username, new BCryptPasswordEncoder().encode(password), "ROLE_USER,ROLE_ADMIN"
-//                ).getBytes()))
-//        );
-//
-//        initializer.setDatabasePopulator(compositeDatabasePopulator);
+        CompositeDatabasePopulator compositeDatabasePopulator = new CompositeDatabasePopulator();
+        compositeDatabasePopulator.addPopulators(new ResourceDatabasePopulator(new ClassPathResource("schema.sql")));
+        compositeDatabasePopulator.addPopulators(new ResourceDatabasePopulator(new ClassPathResource("data.sql")));
+        compositeDatabasePopulator.addPopulators(
+                new ResourceDatabasePopulator(new ByteArrayResource(String.format(
+                        "insert into users values (\'%s\', \'%s\', \'%s\', \'%s\');",
+                        UUID.randomUUID(), username, new BCryptPasswordEncoder().encode(password), "ROLE_USER,ROLE_ADMIN"
+                ).getBytes()))
+        );
+
+        initializer.setDatabasePopulator(compositeDatabasePopulator);
         
         return initializer;
     }
