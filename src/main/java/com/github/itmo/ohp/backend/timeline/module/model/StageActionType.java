67bceba9ecc2b071cd0e4886bd0965f5a8825b76
Dmitriy Solodovnikov <tuzserik@gmail.com>
2023-03-27 07:53:17 +0300

    Revert "Added basic Timeline functionality"
    This reverts commit dfe6c87507a157761be4bb8cb3434d9d89da0847.

--- src/main/java/com/github/itmo/ohp/backend/timeline/module/model/StageActionType.java
+++
@@ -1,8 +0,0 @@
-package com.github.itmo.ohp.backend.timeline.module.model;
-
-public enum StageActionType {
-    NONE,
-    BLOCK_REGISTRATION,
-    BLOCK_GITHUB_SUBMISSION,
-    SHOW_RESULTS
-}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,27 +5,3 @@ insert ignore into info_pages values (
 insert ignore into info_pages values (
     random_uuid(), '{}', false
 );
-
-insert ignore into timeline_stages values
-(
-    random_uuid(),
-    'Registration',
-    'Participant and team registrations are open. Once this stage ends, you will no longer be able to enter the competition.',
-    '2023-03-01 00:00:00',
-    'BLOCK_REGISTRATION'
-),
-(
-    random_uuid(),
-    'Development',
-    'The main stage of the competition. Once this stage ends, you will no longer be able to submit your solution.',
-    '2023-03-10 00:00:00',
-    'BLOCK_GITHUB_SUBMISSION'
-),
-(
-    random_uuid(),
-    'Presentation',
-    'The final stage of the competition where you present your solution. Once this stage ends, the results will be revealed.',
-    '2023-03-13 00:00:00',
-    'SHOW_RESULTS'
-)
-
